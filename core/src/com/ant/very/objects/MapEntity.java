e10859707a292c2bb2856e23edd532dc81c56052
Divoolej <divoolej@gmail.com>
2014-12-18 18:44:36 +0100

    Pre-merge

--- core/src/com/ant/very/objects/MapEntity.java
+++ core/src/com/ant/very/objects/MapEntity.java
@@ -3,5 +3,7 @@ package com.ant.very.objects;
 /**
  * Created by divoolej on 17.12.14.
  */
-public class MapEntity {
+public abstract class MapEntity {
+    abstract void onPick();
+    abstract void onLook();
 }
